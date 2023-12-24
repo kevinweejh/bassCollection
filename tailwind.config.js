ece0becfa9e609f5749642898155635b0513a6e7
Wee Jian Hui Kevin <kevin@Wees-MacBook-Air.local>
2023-12-24 16:52:04 +0800

    feat: Implement Tailwind, complete side panel

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'bg-change': 'bg-change 8s ease-in-out infinite'
+      },
+      keyframes: {
+        'bg-change': {
+          'from': { 'background-color': 'rgba(249, 229, 217, 0.8)' },
+          '16%': { 'background-color': 'rgba(250, 212, 199, 0.8)' },
+          '33%': { 'background-color': 'rgba(199, 217, 218, 0.8)' },
+          '50%': { 'background-color': 'rgba(161, 210, 217, 0.8)' },
+          '66%': { 'background-color': 'rgba(199, 217, 218, 0.8)' },
+          '84%': { 'background-color': 'rgba(250, 212, 199, 0.8)' },
+          'to': { 'background-color': 'rgba(249, 229, 217, 0.8)' }
+        }
+      }
+    },
   },
   plugins: [],
 }
